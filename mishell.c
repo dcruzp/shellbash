0693f2e40fc4c2d94426382022e980c2ea933658
Daniel <danieldelacruzprieto@gmail.com>
2020-10-06 17:31:21 -0400

    ejecuta un comando como ls , pero sin parametros

--- mishell.c
+++ mishell.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 #include <stdio_ext.h>
 #include <string.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
 
 
 #define max_lon 100 
@@ -23,11 +26,38 @@ int main () {
 
         if (strcmp (command , "exit") == 0 )
         {
-            //printf ("%s" , "exit");
-            flag = 0 ;  
+            
+            flag =  0;  
         }
-    }
+        else
+        {
 
+            pid_t process ; 
+
+            process = fork () ; 
+
+            if (process < 0 ) 
+            {
+                perror ("fork") ; 
+                return 2; 
+            }
+            else if (process == 0 )
+            {
+                char * arr [10]; 
+                arr [0] = command ;
+                arr [1] = NULL ; 
+                execvp (arr [0] , arr);
+
+                printf("%s", "this should not print "); 
+            }
+            else 
+            {
+                pid_t wc  = wait(NULL) ; 
+                //printf("%s" ,"test") ;
+            }
+            
+        }        
+    }
     return 0 ; 
 }
 
